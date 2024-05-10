47d54c703bcd6c7e9589ae4178d214fd1a852a0d
salma-thirdwayv <salma.fayez@thirdwayv.com>
2024-05-11 00:50:22 +0300

    first commit

---
+++ src/test/java/iti/spring/testing/lab/ItiSpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package iti.spring.testing.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItiSpringTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
